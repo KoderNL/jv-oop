50816201f192d884a717fd4eec7bc508318f498a
Serhii Nikolaiev <haccent12@gmail.com>
2026-01-13 19:25:00 +0100

    message Truck correction

--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -8,6 +8,6 @@ public class Truck extends Machine {
 
     @Override
     public void stopWork() {
-        System.out.println("Truck machines stopped working");
+        System.out.println("Truck machines stopped working!");
     }
 }
